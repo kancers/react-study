a5267b3116cc9cbcb0b67eef2750577a903e41f2
kancers <kamba.ryosuke@lancers.co.jp>
2020-10-03 22:45:52 +0900

    #72_クラスを表現する

---
+++ 05-class/bird-class.js
@@ -0,0 +1,29 @@
+class Bird {
+    constructor(name) {
+        this.name = name;
+    }
+
+    chirp = () => {
+        console.log(`${this.name}が鳴きました。`);
+    };
+
+    static explain = (name) => {
+        console.log(`${name}は翼があって卵を生みます`);
+    };
+}
+
+class FlyableBird extends Bird {
+    constructor(name) {
+        super(name);
+    }
+
+    fly = () => {
+        console.log(`${this.name}が飛びました。`);
+    };
+}
+
+const penguin = new Bird('ペンギン');
+penguin.chirp();    // ペンギンっが鳴きました。
+Bird.explain('カラス'); // カラスは翼があって卵を生みます
+const hawk = new FlyableBird('タカ'); // タカが飛びました
+hawk.fly();
