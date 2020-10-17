0b25b05e186b5dfd41904b697b8e049882b3b8df
kancers <kamba.ryosuke@lancers.co.jp>
2020-10-17 18:57:21 +0900

    #85_06-object-syntax/deep-copy.js

---
+++ 06-object-syntax/deep-copy.js
@@ -0,0 +1,26 @@
+const patty = {
+    name: 'Patty Rabbit',
+    email: 'patty@maple.town',
+    address : {town: 'Maple Town'},
+};
+
+const rolley = JSON.parse(JSON.stringify(patty));
+rolley.name = 'Rolley Cocker';
+rolley.email = 'rolley@palm.town';
+rolley.address.town = 'Palm Town';
+
+console.log(patty);
+
+// {
+//     name: 'Patty Rabbit',
+//     email: 'patty@maple.town',
+//     address: { town: 'Maple Town' }
+// }
+
+console.log(rolley);
+
+// {
+//     name: 'Rolley Cocker',
+//     email: 'rolley@palm.town',
+//     address: { town: 'Palm Town' }
+// }
