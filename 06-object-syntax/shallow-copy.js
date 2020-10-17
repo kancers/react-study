f4ad4b9ea64f6ce5c21d9cac0eab2b660c6ab0c0
kancers <kamba.ryosuke@lancers.co.jp>
2020-10-17 18:46:57 +0900

    #84_06-object-syntax/shallow-copy.js

---
+++ 06-object-syntax/shallow-copy.js
@@ -0,0 +1,17 @@
+const patty = {
+    name: 'patty Rabbit',
+    email: 'patty@maple.town',
+    address : {town: 'Maple Town'},
+};
+
+const rolley = {...patty, name: 'Rolley Cocker'};
+rolley.email = 'rolley@palm.town';
+rolley.address.town = 'Palm Town';
+
+console.log(patty);
+
+// {
+//     name: 'patty Rabbit',
+//     email: 'patty@maple.town',
+//     address: { town: 'Palm Town' }
+// }
