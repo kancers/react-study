96231c9f5351c1a1e83e186c30f7e799dcb7470d
kancers <kamba.ryosuke@lancers.co.jp>
2020-10-17 17:30:14 +0900

    #79_分割代入とスプレッド構文

---
+++ 06-object-syntax/shorthand.js
@@ -0,0 +1,7 @@
+const keyName = 'bar';
+const baz = 65536;
+const obj1 = { foo: 256, [keyName]: 4096, baz:baz };
+console.log(obj1);  // {foo: 256, bar: 4096, baz: 65536}
+
+const obj2 = { baz };
+console.log(obj2);  // {baz: 65536}
